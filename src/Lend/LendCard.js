7099ab3185ca997a5d466642bdde83a10b772722
bshoop <braxton.shoop@gmail.com>
2021-01-23 19:46:34 -0600

    added image magnification

--- src/Lend/LendCard.js
+++ src/Lend/LendCard.js
@@ -1,10 +1,28 @@
 import React from "react"
 import { ToolCardButtonsContainer } from "./LendDetail"
 import "./Lend.css"
+import ReactImageMagnify from 'react-image-magnify';
 
 export const ToolCard = ({ tool }) => (
     <div className="LendToolsIAmLendingCard">
-        <img className="NewToolPicture" src={tool.toolpicture ? tool.toolpicture : "/Images/ToolMeOnceLogo.jpg.png"} />
+        {/* <img className="NewToolPicture" src={tool.toolpicture ? tool.toolpicture : "/Images/ToolMeOnceLogo.jpg.png"} /> */}
+        <ReactImageMagnify {...{
+            smallImage: {
+                alt: 'Tool Picture',
+                isFluidWidth: false,
+                src: tool.toolpicture ? tool.toolpicture : "/Images/ToolMeOnceLogo.jpg.png",
+                width: 400,
+                height: 325
+            },
+            largeImage: {
+                src: tool.toolpicture ? tool.toolpicture : "/Images/ToolMeOnceLogo.jpg.png",
+                width: 1800,
+                height: 1800
+            }
+        }} />
+
+
+
         <div className="LendToolInfoContainer">
             <ToolCardButtonsContainer tool={tool} />
             <div className="LendToolName"><span className="ToolCardToolName">Tool Name:</span>&nbsp;  {tool.toolname}</div>
